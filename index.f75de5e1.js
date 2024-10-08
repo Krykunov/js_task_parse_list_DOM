79f615a6a75d384cc545f39c8eb3c5477fc192ae
Igor <4430704@gmail.com>
2024-10-08 12:18:38 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+const employeesList = [
+    ...document.querySelectorAll("li")
+];
+const employeesListContainer = document.querySelector("ul");
+const numberFormat = (el)=>Number(el.substring(1).replaceAll(",", ""));
+const sortList = (list)=>{
+    list.sort((a, b)=>numberFormat(b.dataset.salary) - numberFormat(a.dataset.salary));
+    return list;
+};
+const getEmployeesObjects = function(list) {
+    const employees = [];
+    for (const el of list){
+        const employee = {
+            name: el.textContent.replaceAll("\n", "").trim(),
+            position: el.dataset.position,
+            salary: el.dataset.salary,
+            age: el.dataset.age
+        };
+        employees.push(employee);
+    }
+    return employees;
+};
+const sortedList = sortList(employeesList);
+getEmployeesObjects(sortedList);
+employeesListContainer.append(...sortedList);
+
+//# sourceMappingURL=index.f75de5e1.js.map
